4dabeedf1fe4f3cd550f863a64fcc21037a6d230
victor <409097677@qq.com>
2016-11-08 00:50:38 +0800

    use `;` end of stament, use plugins

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import App from './App';
+import Vuex from 'vuex';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+
+// use plugins
+Vue.use(Vuex);
+Vue.use(VueRouter);
+Vue.use(VueResource);
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+});
