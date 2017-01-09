02332432e96b3e0db51b34b3d0da659c90f5873f
kamifun <kamifun409097677@gmail.com>
2017-01-10 00:34:13 +0800

    views/about.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,48 @@
 <template>
   <section class="pt-header">
-    vue-cnode
+    <dl>
+      <dt>关于项目</dt>
+      <dd>
+        使用Cnode.js社区API,基于vue、vue-router、vuex、vue-resource重写node社区的web app
+      </dd>
+    </dl>
+    <dl>
+      <dt>项目源码</dt>
+      <dd>
+        <a href="https://github.com/kamifun/vue-Cnode">https://github.com/kamifun/vue-Cnode</a>
+      </dd>
+    </dl>
+    <dl>
+      <dt>issues反馈</dt>
+      <dd>
+        <a href="https://github.com/kamifun/vue-Cnode/issues">https://github.com/kamifun/vue-Cnode/issues</a>
+      </dd>
+    </dl>
+    <dl>
+      <dt>版本</dt>
+      <dd>
+        v0.0.1
+      </dd>
+    </dl>
   </section>
 </template>
 
 <script>
 export default {
+  name: 'about'
 };
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
+  dl {
+    padding: .5rem .5rem 0;
+    font-size: .4rem;
+    line-height: 1.5;
+    dt {
+      font-weight: bold;
+    }
+    dd {
+      font-size: .3rem;
+    }
+  }
 </style>
